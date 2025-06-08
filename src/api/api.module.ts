6724bf41c433d7de17a072690749a2176820737e
khanghnse181562@fpt.edu.vn <khanghnse181562@fpt.edu.vn>
2025-06-08 17:55:46 +0700

    sth

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { HomeModule } from './home/home.module';
+import { PaymentModule } from './payment/payment.module';
 
 
 @Module({
-  imports: [HomeModule],
+  imports: [HomeModule, PaymentModule],
 })
 export class ApiModule {}
